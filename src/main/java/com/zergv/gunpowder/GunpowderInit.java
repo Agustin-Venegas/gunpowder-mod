89d02300fe9b55bab2e41012a885983165277281
zergv <avenegas19@alumnos.utalca.cl>
2023-03-18 21:20:14 -0300

    First Release Version

--- src/main/java/com/zergv/gunpowder/GunpowderInit.java
+++ src/main/java/com/zergv/gunpowder/GunpowderInit.java
@@ -1,29 +1,22 @@
 package com.zergv.gunpowder;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.*;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class GunpowderInit implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("gunpowder");
 	public static final Item SULFUR = registerItem("sulfur", new Sulfur(new FabricItemSettings()));
-
-	//public static final ItemGroup MISC = FabricItemGroup.builder(new Identifier("gunpowder", "my_item_group_id"))
-    //        .icon(() -> new ItemStack(ModItems.ITEM_NAME))
-    //        .build();
 
 	public static final Item registerItem(String name, Item item)
 	{
